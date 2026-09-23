9c6650eb4d57be0d9e23fc20ddf3fa43f02e6550
zjwt555 <328197661+zjwt555@users.noreply.github.com>
2026-09-23 13:34:28 +0800

    sync: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ============ 魔法小屋 Service Worker：离线可玩 ============ */
-const VERSION = 'magic-house-e1eb8044e0';
+const VERSION = 'magic-house-e49600eedb';
 const CORE = [
   './',
   './index.html',
